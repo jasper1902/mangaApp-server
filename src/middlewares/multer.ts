0f9e4999209bc104110e018055452eb7abb464c1
Jasper2002 <earth1887@gmail.com>
2023-09-18 09:51:54 +0700

    set imagePath

--- src/middlewares/multer.ts
+++ src/middlewares/multer.ts
@@ -1,10 +1,15 @@
 import multer, { diskStorage } from "multer";
 import { v4 as uuidv4 } from "uuid";
 import { Request } from "express";
+import path from "path";
+import fs from "fs";
 
 const storage = diskStorage({
   destination: function (req: Request, file: Express.Multer.File, cb) {
-    cb(null, process.env.IMAGE_PATH as string);
+    const imagePath = path.join(__dirname, "..", "public", "images");
+
+    fs.mkdirSync(imagePath, { recursive: true });
+    cb(null, imagePath);
   },
   filename: function (req: Request, file: Express.Multer.File, cb) {
     const sanitizedName = file.originalname.replace(/ /g, "_");
